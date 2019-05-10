4646249b360485048f414204ebe01d08aea34377
dkumazaw <dkumazaw@stanford.edu>
2019-05-10 10:20:16 +0900

    Add cutout

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 import shutil
 
@@ -41,3 +42,29 @@ def save_checkpoint(state_dict, is_best, savedir, epoch):
     if is_best:
         best_filename = os.path.join(savedir, 'model_best.pt')
         shutil.copyfile(filename, best_filename)
+
+
+class Cutout:
+    """Applies cutout to input image"""
+
+    def __init__(self, length):
+        assert length >= 0
+        self.length = length
+
+    def __call__(self, img):
+        if self.length > 0:
+            h, w = img.size(1), img.size(2)
+            mask = np.ones((h, w), np.float32)
+            y = np.random.randint(h)
+            x = np.random.randint(w)
+
+            y1 = np.clip(y - self.length // 2, 0, h)
+            y2 = np.clip(y + self.length // 2, 0, h)
+            x1 = np.clip(x - self.length // 2, 0, w)
+            x2 = np.clip(x + self.length // 2, 0, w)
+
+            mask[y1: y2, x1: x2] = 0.
+            mask = torch.from_numpy(mask)
+            mask = mask.expand_as(img)
+            img *= mask
+        return img
