893b2bc1a192f3c190b20f029aabde1ccb198798
Daiki Kumazawa <dkumazaw@stanford.edu>
2019-05-11 18:40:29 +0900

    Move multiboxlayer

--- net/ssdlite.py
+++ net/ssdlite.py
@@ -40,41 +40,5 @@ class SSDLite(nn.Module):
     def forward(self, x):
         confs = []
         locs = []
+        pass
 
-
-
-class MultiBoxLayer(nn.Module):
-    def __init__(self, n_classes: int, regression_heads: nn.ModuleList, classification_heads: nn.ModuleList):
-        super(MultiBoxLayer, self).__init__()
-
-        self.n_classes
-        self.regression_heads = regression_heads
-        self.classification_heads = classification_heads
-    
-    def forward(self, hs: list):
-        '''
-        Args:
-            hs: (list) of intermediate layer outputs
-
-        Returns:
-            loc_preds: (tensor) predicted locations, shaped [n, _, 4]
-            conf_preds: (tensor) predicted class confidences, shaped [n, _, n_classes]
-        '''
-        confidences = []
-        locations = []
-        
-        for i, h in enumerate(hs):
-            location = self.regression_heads[i](h)
-            n = location.size(0)
-            location = location.permute(0, 2, 3, 1).contiguous()
-            location = location.view(n, -1, 4)
-            locations.append(location)
-
-            confidence = self.classification_heads[i](h)
-            confidence = confidence.permute(0, 2, 3, 1).contiguous()
-            confidence = confidence.view(n, -1, self.n_classes)
-            confidences.append(confidence)
-        
-        loc_preds  = torch.cat(locations, 1)
-        conf_preds = torch.cat(confidences, 1)
-        return loc_preds, conf_preds
