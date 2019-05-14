0dd8661b5964c9f5874e851d4da62ecb9f967065
dkumazaw <dkumazaw@stanford.edu>
2019-05-14 14:39:34 +0900

    [WIP] Implement ssdlite

--- net/ssdlite.py
+++ net/ssdlite.py
@@ -2,10 +2,12 @@ import torch
 from torch import nn
 
 from .module import Block, SepConv2d
+from .mobilenetv3 import *
 from .multiboxlayer import MultiBoxLayer
 
-
 # Inspired by https://github.com/amdegroot/ssd.pytorch/ and https://github.com/qfgaohao/pytorch-ssd/
+
+
 class SSDLite(nn.Module):
     """SSD Lite
 
@@ -15,9 +17,32 @@ class SSDLite(nn.Module):
     def __init__(self, base_net, n_classes, heads):
         super(SSDLite, self).__init__()
 
+        self._n_classes = n_classes
         self._base_layers = nn.ModuleList()
 
-        self.extras = nn.ModuleList([
+        # [kernel_size, hidden_channels(exp size), in_channels, out_channels(#out), SE, NL, s]
+        self._block_layer_configs = [[3,   16,  16,  16, False, 'RE', 1, False],
+                                     [3,   64,  16,  24, False, 'RE', 2, False],
+                                     [3,   72,  24,  24, False, 'RE', 1, False],
+                                     [5,   72,  24,  40,  True, 'RE', 2, False],
+                                     [5,  120,  40,  40,  True, 'RE', 1, False],
+                                     [5,  120,  40,  40,  True, 'RE', 1, False],
+                                     [3,  240,  40,  80, False, 'HS', 2, False],
+                                     [3,  200,  80,  80, False, 'HS', 1, False],
+                                     [3,  184,  80,  80, False, 'HS', 1, False],
+                                     [3,  184,  80,  80, False, 'HS', 1, False],
+                                     [3,  480,  80, 112,  True, 'HS', 1, False],
+                                     [3,  672, 112, 112,  True, 'HS', 1, False],
+                                     [5,  672, 112, 160,  True, 'HS', 1, True],
+                                     [5,  672, 160, 160,  True, 'HS', 2, False],
+                                     [5,  960, 160, 160,  True, 'HS', 1, False]]
+
+        self._base_layers.append(_gen_init_conv_bn(3, 16, 2))
+
+        for config in self._block_layer_configs:
+            self._base_layers.append(_gen_block_layer(config))
+
+        self._extra_layers = nn.ModuleList([
             Block(in_channels=960, out_channels=512,
                   hidden_channels=int(1280 * 0.2)),
             Block(in_channels=512, out_channels=256,
@@ -27,31 +52,30 @@ class SSDLite(nn.Module):
             Block(in_channels=256, out_channels=64,
                   hidden_channels=int(256 * 0.25))
         ])
-        self.regression_heads = nn.ModuleList([
-            SepConv2d(in_channels=672, out_channels=6 * 4),
-            SepConv2d(in_channels=960, out_channels=6 * 4),
-            SepConv2d(in_channels=512, out_channels=6 * 4),
-            SepConv2d(in_channels=256, out_channels=6 * 4),
-            SepConv2d(in_channels=256, out_channels=6 * 4),
-            nn.Conv2d(in_channels=64, out_channels=6 * 4, kernel_size=1),
-        ])
-        self.classification_heads = nn.ModuleList([
-            SepConv2d(in_channels=672, out_channels=6 * n_classes),
-            SepConv2d(in_channels=960, out_channels=6 * n_classes),
-            SepConv2d(in_channels=512, out_channels=6 * n_classes),
-            SepConv2d(in_channels=256, out_channels=6 * n_classes),
-            SepConv2d(in_channels=256, out_channels=6 * n_classes),
-            nn.Conv2d(in_channels=64, out_channels=6 *
-                      n_classes, kernel_size=1)
-        ])
 
-        self._multibox_layer = MultiBoxLayer(
-            n_classes=n_classes,
-            regression_heads=self.regression_heads,
-            classification_heads=self.classification_heads
-        )
+        self._multibox_layer = MultiBoxLayer(n_classes=self._n_classes)
 
     def forward(self, x):
         confs = []
         locs = []
-        pass
+        hs = []
+        # Forward pass through base layers
+        for index, layer in enumerate(self._base_layers[:-1]):
+            if index == 13:
+                # C4 layer
+                x, h = layer(x)
+                hs.append(h)
+                continue
+
+            x = layer(x)
+
+        # Last layer in base layers
+        x = self._base_layers[-1](x)
+        hs.append(x)
+
+        # Forward pass through extra layers
+        for layer in self._extra_layers:
+            x = layer(x)
+            hs.append(x)
+
+        loc_preds, conf_preds = self._multibox_layer(hs)
